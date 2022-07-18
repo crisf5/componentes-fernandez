adb616fbe162d1530d0dbcca1370fb6161374c35
crisf5 <cristianfcba@gmail.com>
2022-07-18 14:39:26 -0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AreaComponent } from './components/area/area.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AreaComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
